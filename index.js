6d0d2b6f37c5d81ce6616f134a0967cd1fd18073
Samantha Ambroise <ilkigin@gmail.com>
2019-02-02 13:53:37 -0500

    added toggleclass feature

--- index.js
+++ index.js
@@ -32,12 +32,17 @@ function deleteItem(){
     })
 }
 
+function checkItem(){
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+        
+    })
+}
+
 function shoppingList(){
     addItem();
     deleteItem();
+    checkItem();
 }
 
 $(shoppingList);
-
-
-
